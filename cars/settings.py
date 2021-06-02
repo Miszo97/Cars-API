a2a264c6baed7e4b9987fdb7dfd3164bfbed15b4
Artur <kol478@gmail.com>
2021-06-02 17:41:23 +0200

    Add applications to installed apps

--- cars/settings.py
+++ cars/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cars_api.apps.CarsApiConfig',
+    'rest_framework',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
