0617fca989f3397edae282a3b112d7dae29fb1c7
Artur <kol478@gmail.com>
2021-06-04 22:38:11 +0200

    Add new routings to urls

--- cars_api/urls.py
+++ cars_api/urls.py
@@ -1,9 +1,16 @@
+from django.urls import path
 from rest_framework import routers
 
-from cars_api.views import CarViewSet
+from cars_api.views import CarViewSet, PopularView, RateView
 
 app_name = 'cars_api'
 
-router = routers.SimpleRouter()
-router.register(r'cars', CarViewSet)
-urlpatterns = router.urls
+router = routers.SimpleRouter(trailing_slash=False)
+router.register(r'cars', CarViewSet, basename='cars')
+
+urlpatterns = [
+    path('rate', RateView.as_view(), name='rate'),
+    path('popular', PopularView.as_view(), name='popular'),
+]
+
+urlpatterns += router.urls
