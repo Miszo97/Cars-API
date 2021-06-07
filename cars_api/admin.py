05c135d98d15eb423a6e9432745ddee542f3e7d3
Artur <kol478@gmail.com>
2021-06-07 15:19:42 +0200

    Register models in admin site

--- cars_api/admin.py
+++ cars_api/admin.py
@@ -1,1 +1,11 @@
 # Register your models here.
+
+from django.contrib import admin
+
+from cars_api.models import Car, Rate
+
+# Register your models here.
+
+
+admin.site.register(Car)
+admin.site.register(Rate)
